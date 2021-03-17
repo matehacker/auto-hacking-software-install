5b391b9fae5b030102614ff2b1c3712330cabfa3
matehacker <57183267+matehacker@users.noreply.github.com>
2021-03-17 15:33:22 +0100

    Add files via upload

---
+++ hacking software installer.sh
@@ -0,0 +1,29 @@
+echo rendszer frissítése
+sudo apt ugrade
+sudo apt update
+echo hacker eszközök telepítése
+sudo apt install nmap
+sudo apt install metasploitable framework
+sudo apt install beef
+sudo apt install hydra
+sudo apt install Tor
+sudo apt install reaver
+sudo apt install aircrack-ng
+sudo apt install netdiscover
+sudo apt install Powersploit
+sudo apt install john
+sudo apt install pyrit
+sudo apt install yara
+sudo apt install shellnoob
+sudo apt install apktool
+sudo apt install NASM shell
+sudo apt install httrack
+sudo apt install recon-ng
+sudo apt install ike scan
+sudo apt install javasnoob
+sudo apt install dex2jar
+sudo apt install jad
+sudo apt install radare2
+echo rendszer frissítése
+sudo apt update
+echo a telepítése befejeződött
